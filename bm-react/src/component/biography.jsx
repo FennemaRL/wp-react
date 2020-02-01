9048902813c346cbf434ee96e53a61e9aef28d77
Rodriguez fennema lucas <ld.fennema@gmail.com>
2020-01-31 21:03:47 -0300

    modificacion de biografia

--- bm-react/src/component/biography.jsx
+++ bm-react/src/component/biography.jsx
@@ -13,53 +13,72 @@ import icokanami from "../img/members/icokanami.png";
 import icomisa from "../img/members/icomisa.png";
 import icoakane from "../img/members/icoakane.png";
 import icomiku from "../img/members/icokuruppo.png";
+
+import ig from "../img/socialMediaIcon/instagram.svg";
+import twt from "../img/socialMediaIcon/twitter.svg";
+
 class Biography extends Component {
   state = {
     members: [
       {
-        name: "Saiki",
+        name: "Saiki Atsumi",
         image: saikimg,
-        description: "",
+        description:
+          "With a cool and badass demeanour, she usually sports an entirely black maid uniform with a blue rose in her hair." +
+          " She may look like a maid but she won’t give you a maid greeting and she certainly doesn’t have a Maid ‘charm point’.",
+        story:
+          "Saiki is the last member joined this group through an audition in August of 2013",
         ico: icosaiki,
         roll: "Main voice",
-        twt: "saiki_bandmaid",
-        ig: "saiki_bandmaid/"
+        twt: "https://twitter.com/saiki_bandmaid",
+        ig: "www.instagram.com/saiki_bandmaid/"
       },
       {
-        name: "Kanami",
+        name: "Kanami Tono",
         image: kanaimg,
-        description: "",
+        description:
+          "Kanami usually wears a black and white maid outfit with a lace bonnet and prefers to be called Kanamincho (or Mincho, for short)." +
+          " The band members describe Kanami as a strange, natural airhead, clueless to situations and she’s just as you see her",
+        story: "She was the first member recruited by miku through an audition",
         ico: icokanami,
         roll: "Main guitar",
-        twt: "saiki_bandmaid",
-        ig: "saiki_bandmaid/"
+        twt: "https://twitter.com/kanami_bandmaid",
+        ig: "https://www.instagram.com/kanami_bandmaid/"
       },
       {
-        name: "Miku",
+        name: "Kobato Miku",
         image: mikuimg,
-        description: "",
+        description:
+          "She usually sports a traditional white and black maid outfit, with a red rose in her hair, and admits to goofing off regularly during rehearsals." +
+          "Always said that herself is a pigeon, and usually end her line with -po!, with her “Kuruppo!” catchphrase.",
+        story:
+          "Miku had the idea of having a band while working at a maid cafe and that idea became Band-Maid",
         ico: icomiku,
         roll: "Voice/Guitar",
-        twt: "saiki_bandmaid",
-        ig: "saiki_bandmaid/"
+        twt: "https://twitter.com/miku_bandmaid",
+        ig: "https://www.instagram.com/kobatomiku/"
       },
       {
         name: "Misa",
         image: misaimg,
-        description: "",
+        description:
+          "She wears long black dresses and is known to like her whiskey",
         ico: icomisa,
+        story: "Misa was suggested by akane to join the band",
         roll: "Bass",
-        twt: "saiki_bandmaid",
-        ig: "saiki_bandmaid/"
+        twt: "https://twitter.com/misa_bandmaid",
+        ig: "www.instagram.com/misa_bandmaid/"
       },
       {
-        name: "Akane",
+        name: "Akane Hirose",
         image: akaneimg,
-        description: "",
+        description:
+          " She usually wears a black and white maid uniform, as well as a small hat. Her Maid ‘charm point’ is that she has a really big appetite",
         ico: icoakane,
+        story: "Akane whas suggested by kanami to join the band",
         roll: "Drums",
-        twt: "saiki_bandmaid",
-        ig: "saiki_bandmaid/"
+        twt: "https://twitter.com/achi_bandmaid",
+        ig: "https://www.instagram.com/akane_bandmaid/"
       }
     ]
   };
@@ -99,11 +118,32 @@ class Biography extends Component {
                   />
                 </div>
                 <div className="composeImgWithDescription">
-                  <img src={member.image} alt="a member of band maid" />
+                  <img
+                    className="photo"
+                    src={member.image}
+                    alt="a member of band maid"
+                  />
                   <div className="texthp">
-                    <h5>roll : {member.roll}</h5>
-                    <p> description : {member.description}</p>
-                    <p> social media :</p>
+                    <h5>Roll : {member.roll} </h5>
+                    <p>Join : {member.story} </p>
+                    <p>Description : {member.description} </p>
+                    <p>
+                      Social media :
+                      <a href={member.ig}>
+                        <img
+                          src={ig}
+                          alt="link to instagram socialmedia"
+                          className="socialMediaIcon"
+                        ></img>
+                      </a>
+                      <a href={member.twt}>
+                        <img
+                          className="socialMediaIcon"
+                          src={twt}
+                          alt="link to twitter socialmedia"
+                        ></img>
+                      </a>
+                    </p>
                   </div>
                 </div>
               </div>
