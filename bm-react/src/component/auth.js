9bc842f8ce55d27236c884a787b46ab075a1caa1
Rodriguez fennema lucas <ld.fennema@gmail.com>
2020-02-02 21:18:33 -0300

    falta refrescar token y estilos de discografia

---
+++ bm-react/src/component/auth.js
@@ -0,0 +1,28 @@
+async function tokenSpotify() {
+  if (!localStorage.getItem("tokenBM")) {
+    let key = process.env.REACT_APP_SK;
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", `Basic ${key}`);
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    let urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "client_credentials");
+
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow"
+    };
+    fetch("https://accounts.spotify.com/api/token", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        let res = JSON.parse(result);
+        localStorage.setItem("tokenBM", res.access_token);
+        console.log("ya se guardo");
+        console.log(res.access_token);
+      })
+      .catch(error => console.error(error));
+  }
+}
+export default tokenSpotify;
--- bm-react/src/component/discography.jsx
+++ bm-react/src/component/discography.jsx
@@ -1,36 +1,113 @@
 import React, { Component } from "react";
+import tokenSpotify from "./auth";
 class Discography extends Component {
-  state = {};
+  state = { albums: [] };
 
-  componentDidMount() {
-    let key = process.env.REACT_APP_SK;
+  async getAlbums(artists, firstTry = true) {
+    console.log("inicio");
+    let key = localStorage.getItem("tokenBM");
+    firstTry
+      ? console.log(
+          "antes valor k primera ves " +
+            key +
+            "valor localstorage " +
+            localStorage.getItem("tokenBM")
+        )
+      : console.log(
+          "antes valor k 2 ves " +
+            key +
+            "valor localstorage " +
+            localStorage.getItem("tokenBM")
+        );
+    key =
+      localStorage.getItem("tokenBM") ||
+      (!(await tokenSpotify()) &&
+        localStorage.getItem(
+          "tokenBM"
+        )); /*BQBpPT2mA7QBxPqs9PWEpnBJnUKsaJ4zc2g0Tj3v79quiul2xtGiGr08QsCBvC-xM3PzPqz6QELFzPLcbLw*/
+    firstTry
+      ? console.log(
+          "despues valor k primera ves " +
+            key +
+            "valor localstorage " +
+            localStorage.getItem("tokenBM")
+        )
+      : console.log(
+          "despues valor k 2 ves " +
+            key +
+            "valor localstorage " +
+            localStorage.getItem("tokenBM")
+        );
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Basic ${key}`);
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("grant_type", "client_credentials");
+    myHeaders.append("Authorization", `Bearer ${key}`);
 
     var requestOptions = {
-      method: "POST",
+      method: "GET",
       headers: myHeaders,
-      body: urlencoded,
       redirect: "follow"
     };
-
-    fetch("https://accounts.spotify.com/api/token", requestOptions)
+    fetch(
+      `https://api.spotify.com/v1/artists/${artists}/albums`,
+      requestOptions
+    )
       .then(response => response.text())
       .then(result => {
-        this.setState({ result });
-        console.log(result);
+        let resultobj = JSON.parse(result);
+        if (resultobj.error) {
+          throw resultobj.error;
+        }
+        let albumsd = resultobj.items;
+        console.log(albumsd);
+        this.setState({
+          albums: this.state.albums.concat(
+            albumsd.reduce(
+              (unique, item) =>
+                unique.map(i => i.name).includes(item.name)
+                  ? unique
+                  : [...unique, item],
+              []
+            )
+          )
+        });
       })
-      .catch(error => console.log("error", error));
+      .catch(error => {
+        console.log("error");
+        console.log(error);
+        if (
+          error.status === 401 &&
+          error.message === "The access token expired"
+        ) {
+          console.log("entre wey");
+          localStorage.removeItem("tokenBM");
+          firstTry
+            ? this.getAlbums(artists, false)
+            : /*for refresh the token only 1 time */
+              console.log("no se refresca mas:(");
+        }
+      });
+  }
+
+  componentDidMount() {
+    /*
+    this.getAlbums("5Wh3G01Xfxn2zzEZNpuYHH");
+    this.getAlbums("2OIN4qI2EqAsEhrVlnfi02");*/
+    this.getAlbums("7k73EtZwoPs516ZxE72KsO");
   }
   render() {
     return (
       <div className="container">
         <h1>Discography</h1>
-        <div className="principal disco"></div>
+        <div className="principal disco">
+          {this.state.albums.map(album => {
+            return (
+              <div key={album.name + " " + Date.now()}>
+                <p>{album.name}</p>
+                <img src={album.images[2].url} alt="" />
+                <p>{album.release_date}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
