ce5a71b3cfaa5d4e1373d24ab7f586be40dc25cc
Fennema lucas <ld.fennema@gmail.com>
2020-08-15 14:11:08 -0300

    modificacion del resize del video

--- src/component/home/latestvideo.jsx
+++ src/component/home/latestvideo.jsx
@@ -1,58 +1,59 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import YouTube from "react-youtube";
 
-class LatestVideo extends Component {
-  state = {
-    videos: [],
-    videoSize: {
-      height:
-        window.innerWidth <= 768
-          ? (window.innerHeight / 2).toString + "px"
-          : "430px",
-      width: window.innerWidth <= 768 ? "100%" : "60%",
-      playerVars: {
-        autoplay: 0
-      },
-      min: window.innerWidth <= 768
+let configReprod ={
+  playerVars: {autoplay: 0},
+  min: window.innerWidth < 768
+ }
+function LatestVideo(){
+  
+  const [videos, setVideos] = useState([])
+  const [dimensions, setDimensions] = useState({ 
+    height: "400px",
+    width: "60%"
+  })
+  
+  useEffect(()=>{
+    function handleResize() {
+      setDimensions({
+        height: window.innerWidth < 768 ?  300 : 400 ,
+        width: window.innerWidth < 768 ? "95%" :"60%",
+        min: window.innerWidth < 768
+      })
     }
-  };
-
-  componentDidMount() {
-    if (!this.state.videos.size) {
-      let kg = process.env.REACT_APP_GK;
-      let bmuid = process.env.REACT_APP_bmuid;
+      window.addEventListener('resize', handleResize)
+    
+      return ()=> {
+        window.removeEventListener('resize', handleResize)
+      }
+    },[])
+  useEffect(()=>{
+    let kg = process.env.REACT_APP_GK;
+    let bmuid = process.env.REACT_APP_bmuid;
       axios
         .get(
           `https://www.googleapis.com/youtube/v3/search?key=${kg}&channelId=${bmuid}&part=snippet,id&order=date&maxResults=1&showinfo=0&enablejsapi=1`
         )
         .then(res => {
-          this.setState({ videos: res.data.items });
+          setVideos(res.data.items )
         })
         .catch(e => () => {});
-    }
-  }
-
-  render() {
-    const videos = this.state.videos;
-    const config = this.state.videoSize;
-    return (
+  },[])
+  return (
       <div>
         <h1>Latest Video</h1>
-          {videos.length &&
-            videos.map(video => {
-              return (
+          {!!videos.length &&
+            videos.map(video => (
                 <YouTube
                   videoId={video.id.videoId}
-                  opts={config}
-                  onReady={this._onReady}
+                  opts={{...configReprod, width:dimensions.width, height:dimensions.height, min:dimensions.min}}
                   key={video.id.videoId}
                 />
-              );
-            })}
+              )
+            )}
       </div>
-    );
-  }
+    )
 }
 
 export default LatestVideo;
